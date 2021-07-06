30118a6b91274fb00070a8bd10160698731ea4d9
NyCode <nico@nycode.de>
2021-07-06 11:37:40 +0000

    Remove containers

--- .space.kts
+++ .space.kts
@@ -1,21 +1,17 @@
-job("Build and Publish API") {
-    container(displayName = "Build and Publish Nyper-API", image = "cimg/openjdk:16.0") {
-        env["JFROG_USERNAME"] = Params("jfrog_username")
-        env["JFROG_PASSWORD"] = Secrets("jfrog_password")
-        env["SIGNING_KEY"] = Secrets("signing_key")
-        env["SIGING_PASSWORD"] = Secrets("signing_password")
-        kotlinScript { api ->
-            api.gradlew("applyPatches")
-            api.gradlew("publish")
-        }
+job("Build and Publish API") {{
+    env["JFROG_USERNAME"] = Params("jfrog_username")
+    env["JFROG_PASSWORD"] = Secrets("jfrog_password")
+    env["SIGNING_KEY"] = Secrets("signing_key")
+    env["SIGING_PASSWORD"] = Secrets("signing_password")
+    kotlinScript { api ->
+        api.gradlew("applyPatches")
+        api.gradlew("publish")
     }
 }
 
 job("Build Paperclip Jar") {
-    container(displayName = "Build Nyper Paperclip Jar", image = "cimg/openjdk:16.0") {
-        kotlinScript { api ->
-            api.gradlew("applyPatches")
-            api.gradlew("paperclipJar")
-        }
+    kotlinScript { api ->
+        api.gradlew("applyPatches")
+        api.gradlew("paperclipJar")
     }
 }
