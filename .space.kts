6ffbfe94648e9adbf2b5447853e25c92318bef0e
NyCode <nico@nycode.de>
2021-07-07 14:20:57 +0000

    Revert "Remove containers"
    This reverts commit 30118a6b91274fb00070a8bd10160698731ea4d9.

--- .space.kts
+++ .space.kts
@@ -1,17 +1,21 @@
-job("Build and Publish API") {{
-    env["JFROG_USERNAME"] = Params("jfrog_username")
-    env["JFROG_PASSWORD"] = Secrets("jfrog_password")
-    env["SIGNING_KEY"] = Secrets("signing_key")
-    env["SIGING_PASSWORD"] = Secrets("signing_password")
-    kotlinScript { api ->
-        api.gradlew("applyPatches")
-        api.gradlew("publish")
+job("Build and Publish API") {
+    container(displayName = "Build and Publish Nyper-API", image = "cimg/openjdk:16.0") {
+        env["JFROG_USERNAME"] = Params("jfrog_username")
+        env["JFROG_PASSWORD"] = Secrets("jfrog_password")
+        env["SIGNING_KEY"] = Secrets("signing_key")
+        env["SIGING_PASSWORD"] = Secrets("signing_password")
+        kotlinScript { api ->
+            api.gradlew("applyPatches")
+            api.gradlew("publish")
+        }
     }
 }
 
 job("Build Paperclip Jar") {
-    kotlinScript { api ->
-        api.gradlew("applyPatches")
-        api.gradlew("paperclipJar")
+    container(displayName = "Build Nyper Paperclip Jar", image = "cimg/openjdk:16.0") {
+        kotlinScript { api ->
+            api.gradlew("applyPatches")
+            api.gradlew("paperclipJar")
+        }
     }
 }
