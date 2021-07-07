e6cdf863bf6225762c57fdea9a58e6c69ea8331a
NyCode <nico@nycode.de>
2021-07-07 14:21:10 +0000

    Revert "Use CircleCI openjdk image"
    This reverts commit d00860186569f16de12b87ee40be26dc16e25198.

--- .space.kts
+++ .space.kts
@@ -1,5 +1,5 @@
 job("Build and Publish API") {
-    container(displayName = "Build and Publish Nyper-API", image = "cimg/openjdk:16.0") {
+    container(displayName = "Build and Publish Nyper-API", image = "adoptopenjdk:16") {
         env["JFROG_USERNAME"] = Params("jfrog_username")
         env["JFROG_PASSWORD"] = Secrets("jfrog_password")
         env["SIGNING_KEY"] = Secrets("signing_key")
@@ -12,7 +12,7 @@ job("Build and Publish API") {
 }
 
 job("Build Paperclip Jar") {
-    container(displayName = "Build Nyper Paperclip Jar", image = "cimg/openjdk:16.0") {
+    container(displayName = "Build Nyper Paperclip Jar", image = "adoptopenjdk:16") {
         kotlinScript { api ->
             api.gradlew("applyPatches")
             api.gradlew("paperclipJar")
